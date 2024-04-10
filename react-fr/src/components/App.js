76d8429227cc2aff9a8d348d1dd6f5882e398cfe
Zaw Ye Htet Naing <zawye.official20@gmail.com>
2024-04-10 16:36:36 +0900

    データアップデートとフィルターテスト

--- react-fr/src/components/App.js
+++ react-fr/src/components/App.js
@@ -22,51 +22,48 @@ import { userType as  DefinedUserType } from '../data';
  */
 const data = [
   {
-    name: {
-      firstName: 'John',
-      lastName: 'Doe',
-    },
-    address: '261 Erdman Ford',
-    city: 'East Daphne',
-    state: 'Kentucky',
+    name: "John",
+    fullname: "JohnDoe",
+    company: '261 Erdman Ford',
+    memberID: '111',
+    formerID: '10001',
+    mailAddress: 'JD@erd.co',
+    startYM: '2024/04',
+    endYM: '2024/12'
   },
   {
-    name: {
-      firstName: 'Jane',
-      lastName: 'Doe',
-    },
-    address: '769 Dominic Grove',
-    city: 'Columbus',
-    state: 'Ohio',
+    name: "Jane",
+    fullname: "Jane Doe",
+    company: '261 Erdman Ford',
+    memberID: '112',
+    formerID: '10002',
+    mailAddress: 'Jane@erd.co',
+    startYM: '2024/04',
+    endYM: '2024/12'
   },
   {
-    name: {
-      firstName: 'Joe',
-      lastName: 'Doe',
-    },
-    address: '566 Brakus Inlet',
-    city: 'South Linda',
-    state: 'West Virginia',
+    name: "Aung",
+    fullname: "Than Aung",
+    company: 'Freedom Exp.',
+    memberID: '113',
+    formerID: 'HJ009',
+    mailAddress: 'ta2020@free.net',
+    startYM: '2024/04',
+    endYM: '2025/12'
   },
   {
-    name: {
-      firstName: 'Kevin',
-      lastName: 'Vandy',
-    },
-    address: '722 Emie Stream',
-    city: 'Lincoln',
-    state: 'Nebraska',
-  },
-  {
-    name: {
-      firstName: 'Joshua',
-      lastName: 'Rolluffs',
-    },
-    address: '32188 Larkin Turnpike',
-    city: 'Charleston',
-    state: 'South Carolina',
+    name: "Satou",
+    fullname: "Satou",
+    company: 'Gfords',
+    memberID: '114',
+    formerID: 'HHH20',
+    mailAddress: 'sky@hotmail.com',
+    startYM: '2024/04',
+    endYM: '2025/12'
   }
 ];
+
+const companySelect = data.map(d => d.company);
 
 function App({userType}) {
   //should be memoized or stable
@@ -79,16 +76,17 @@ function App({userType}) {
   const columns = useMemo(
     () => [
       {
-        accessorKey: 'name.firstName', //access nested data with dot notation
-        header: 'First Name',
+        accessorKey: 'name', //access nested data with dot notation
+        header: '名前',
+        filterVariant: 'text',
         size: 150,
         Cell: ({cell})=>{
           return (<span style={{color:'red'}}>{cell.getValue()}</span>)
         }
       },
       {
-        accessorKey: 'name.lastName',
-        header: 'Last Name',
+        accessorKey: 'fullname',
+        header: 'Name',
         size: 150,
         Footer: (obj) =>{
           console.log("logging 2",obj);
@@ -96,13 +94,15 @@ function App({userType}) {
         }
       },
       {
-        accessorKey: 'address', //normal accessorKey
-        header: 'Address',
+        accessorKey: 'company', //normal accessorKey
+        header: '会社',
+        filterVariant: 'select',
+        filterSelectOptions: companySelect,
         size: 200,
       },
       {
-        accessorKey: 'city',
-        header: 'City',
+        accessorKey: 'memberID',
+        header: '社員ID',
         size: 150,
         muiTableHeadCellProps: {
           align: 'center',
@@ -112,8 +112,8 @@ function App({userType}) {
         },
       },
       {
-        accessorKey: 'state',
-        header: 'State',
+        accessorKey: 'formerID',
+        header: '旧社員ID',
         size: 150,
         muiTableHeadCellProps: {
           align: 'center',
@@ -121,6 +121,22 @@ function App({userType}) {
         muiTableBodyCellProps: {
           align: 'center',
         },
+      },
+      {
+        accessorKey: 'mailAddress', //normal accessorKey
+        header: 'メールアドレス',
+        size: 200,
+      },
+      {
+        accessorKey: 'startYM', //normal accessorKey
+        header: '配属日',
+        size: 200,
+        filterVariant: "date-range"
+      },
+      {
+        accessorKey: 'endYM', //normal accessorKey
+        header: '退出日',
+        size: 200,
       },
     ],
     []
@@ -129,7 +145,10 @@ function App({userType}) {
   const table = useMaterialReactTable({
     columns, 
     data, 
-    initialState: { columnVisibility: { state: userType === DefinedUserType.admin ? true : false } },
+    initialState: { 
+      columnVisibility: { endYM: userType === DefinedUserType.admin ? true : false },
+      showColumnFilters: true,
+    },
     enableRowActions: true,
     displayColumnDefOptions: { 'mrt-row-actions': { size: 120 } },
     renderRowActions: ({row}) =>{
